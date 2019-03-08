15f7cceb45ee10fefe70b7250ab51cac483bac9d
HERNANDEZ ANGELES PAOLA CITLALI <Z805693@isban.mx>
2019-03-07 19:55:31 -0600

    formularios y routing

--- src/app/services/articulos.service.ts
+++ src/app/services/articulos.service.ts
@@ -16,6 +16,14 @@ export class ArticulosService {
     return this.articulos;
   }
 
+public get(id:number){
+  let articulo: Articulo =  this.articulos.find((item:Articulo)=> {
+    return item.id==id;
+  })
+  return articulo;
+}
+
+
   public nuevo(articulo: Articulo) {
     this.articulos.push(articulo);
   }
